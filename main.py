1185e2d59fbffcb30b1f5a9fc518ecacf36420eb
w_kelvin <waringakelvin4@gmail.com>
2024-06-08 23:41:14 +0300

    Add Python script for tracking cycling habits using Pixela API

---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+from datetime import datetime
+
+USERNAME = "waringa"
+TOKEN = "lkjhgfdsa"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "shibafu"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/"
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometres did you cycle today? ")
+}
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+new_pixel_data = {
+    "quantity": "15"
+}
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
+
+
